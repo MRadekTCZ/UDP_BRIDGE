b4ee9d4b33d4ce04de3cfdb3bd52d611fe316a98
MRadekTCZ <maciekztczewa@gmail.com>
2023-10-26 10:45:16 +0200

    udalo sie utworzyc stabilne polaczenie. Firewall musi byc wylaczony. Dziala na wersji stm ide 1.10, na 1.13 nie chce dzialac

--- STM32/UDP/CM7/Debug/Drivers/BSP/Components/lan8742.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/lan8742.o: \
- C:/Users/Maciek/Desktop/Rozwoj/UDP_BRIDGE/UDP_BRIDGE/UDP_BRIDGE/STM32/UDP/Drivers/BSP/Components/lan8742/lan8742.c \
- C:/Users/Maciek/Desktop/Rozwoj/UDP_BRIDGE/UDP_BRIDGE/UDP_BRIDGE/STM32/UDP/Drivers/BSP/Components/lan8742/lan8742.h
-C:/Users/Maciek/Desktop/Rozwoj/UDP_BRIDGE/UDP_BRIDGE/UDP_BRIDGE/STM32/UDP/Drivers/BSP/Components/lan8742/lan8742.h:
