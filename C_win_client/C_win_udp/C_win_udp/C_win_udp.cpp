e093d43c98f67b98e15babe2db341cbf79851351
MRadekTCZ <maciekztczewa@gmail.com>
2023-10-28 13:04:19 +0200

    dziala wysylanie i odbieranie danych z klienta UDP

--- C_win_client/C_win_udp/C_win_udp/C_win_udp.cpp
+++ C_win_client/C_win_udp/C_win_udp/C_win_udp.cpp
@@ -1,10 +1,10 @@
 ﻿#include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <string>
 #include <thread>
+#include <string>
 #pragma comment(lib, "ws2_32.lib")
-
+#define TWÓJ_WYBRANY_PORT 723
 int main() {
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
@@ -19,10 +19,23 @@ int main() {
         return 1;
     }
 
+    sockaddr_in clientAddr;
+    clientAddr.sin_family = AF_INET;
+    clientAddr.sin_port = htons(TWÓJ_WYBRANY_PORT); // Tutaj wpisz wybrany numer portu dla klienta
+    clientAddr.sin_addr.s_addr = INADDR_ANY;
+
+    // Przypisz numer portu do gniazda klienta
+    if (bind(clientSocket, (struct sockaddr*)&clientAddr, sizeof(clientAddr)) == SOCKET_ERROR) {
+        std::cerr << "Błąd podczas przypisywania numeru portu klientowi." << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
     sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(4077); // numer portu serwera
-    if (inet_pton(AF_INET, "192.168.3.59", &(serverAddr.sin_addr)) <= 0) {
+    if (inet_pton(AF_INET, "192.168.3.247", &(serverAddr.sin_addr)) <= 0) {
         std::cerr << "Błąd konwersji adresu IP." << std::endl;
         closesocket(clientSocket);
         WSACleanup();
@@ -40,10 +53,22 @@ int main() {
         }
         else {
             std::cout << "Wysłano " << bytesSent << " bajtów danych: " << message << std::endl;
+
+            char buffer[1024];
+            int addrLen = sizeof(serverAddr);
+            int bytesReceived = recvfrom(clientSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&serverAddr, &addrLen);
+
+            if (bytesReceived > 0) {
+                buffer[bytesReceived] = '\0';
+                std::cout << "Odebrano: " << buffer << std::endl;
+            }
+            else {
+                std::cerr << "Błąd podczas odbierania danych: " << WSAGetLastError() << std::endl;
+            }
         }
 
         ++messageCounter;
-        Sleep(1000); // Opóźnienie programu na 1 sekundę
+        Sleep(5000); // Odczekaj 5 sekund przed wysłaniem kolejnej wiadomości
     }
 
     closesocket(clientSocket);
