3a0adcab863499a291d9105794c443256f62b7cf
MRadekTCZ <maciekztczewa@gmail.com>
2023-10-28 14:29:46 +0200

    dziala wysylanie i odbieranie danych z klienta UDP v2

--- C_win_client/C_win_udp/C_win_udp/C_win_udp.cpp
+++ C_win_client/C_win_udp/C_win_udp/C_win_udp.cpp
@@ -3,8 +3,33 @@
 #include <ws2tcpip.h>
 #include <thread>
 #include <string>
+
 #pragma comment(lib, "ws2_32.lib")
-#define TWÓJ_WYBRANY_PORT 723
+
+#define CLIENT_PORT 723
+#define SERVER_PORT 4077
+#define SERVER_IP "192.168.3.247"
+
+// Funkcja obsługująca odbieranie odpowiedzi od serwera w osobnym wątku
+void ReceiveThread(SOCKET clientSocket) {
+    char buffer[1024];
+    sockaddr_in serverAddr;
+    int addrLen = sizeof(serverAddr);
+
+    while (true) {
+        int bytesReceived = recvfrom(clientSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&serverAddr, &addrLen);
+
+        if (bytesReceived > 0) {
+            buffer[bytesReceived] = '\0';
+            std::cout << "Odebrano: " << buffer << std::endl;
+        }
+        else {
+            std::cerr << "Błąd podczas odbierania danych: " << WSAGetLastError() << std::endl;
+            break;
+        }
+    }
+}
+
 int main() {
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
@@ -21,12 +46,12 @@ int main() {
 
     sockaddr_in clientAddr;
     clientAddr.sin_family = AF_INET;
-    clientAddr.sin_port = htons(TWÓJ_WYBRANY_PORT); // Tutaj wpisz wybrany numer portu dla klienta
+    clientAddr.sin_port = htons(CLIENT_PORT); // Wybierz dowolny numer portu klienta
     clientAddr.sin_addr.s_addr = INADDR_ANY;
 
     // Przypisz numer portu do gniazda klienta
     if (bind(clientSocket, (struct sockaddr*)&clientAddr, sizeof(clientAddr)) == SOCKET_ERROR) {
-        std::cerr << "Błąd podczas przypisywania numeru portu klientowi." << std::endl;
+        std::cerr << "Error in client port" << std::endl;
         closesocket(clientSocket);
         WSACleanup();
         return 1;
@@ -34,42 +59,36 @@ int main() {
 
     sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(4077); // numer portu serwera
-    if (inet_pton(AF_INET, "192.168.3.247", &(serverAddr.sin_addr)) <= 0) {
-        std::cerr << "Błąd konwersji adresu IP." << std::endl;
+    serverAddr.sin_port = htons(SERVER_PORT); // Numer portu serwera
+    if (inet_pton(AF_INET, SERVER_IP, &(serverAddr.sin_addr)) <= 0) {
+        std::cerr << "Error IP." << std::endl;
         closesocket(clientSocket);
         WSACleanup();
         return 1;
     }
 
-    int messageCounter = 1;
+    // Uruchom wątek do obsługi odbierania odpowiedzi od serwera
+    std::thread receiveThread(ReceiveThread, clientSocket);
 
     while (true) {
-        std::string message = "message " + std::to_string(messageCounter);
+        std::cout << "Message to send (HEX) - od press 'q' to leave";
+        std::string message;
+        std::cin >> message;
+
+        if (message == "q") {
+            break;
+        }
 
         int bytesSent = sendto(clientSocket, message.c_str(), message.length(), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
         if (bytesSent == SOCKET_ERROR) {
-            std::cerr << "Błąd podczas wysyłania danych: " << WSAGetLastError() << std::endl;
-        }
-        else {
-            std::cout << "Wysłano " << bytesSent << " bajtów danych: " << message << std::endl;
-
-            char buffer[1024];
-            int addrLen = sizeof(serverAddr);
-            int bytesReceived = recvfrom(clientSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&serverAddr, &addrLen);
-
-            if (bytesReceived > 0) {
-                buffer[bytesReceived] = '\0';
-                std::cout << "Odebrano: " << buffer << std::endl;
-            }
-            else {
-                std::cerr << "Błąd podczas odbierania danych: " << WSAGetLastError() << std::endl;
-            }
+            std::cerr << "Error while sending the data: " << WSAGetLastError() << std::endl;
         }
 
-        ++messageCounter;
-        Sleep(5000); // Odczekaj 5 sekund przed wysłaniem kolejnej wiadomości
+        std::this_thread::sleep_for(std::chrono::seconds(1)); // Odczekaj 1 sekundę przed kolejnym wprowadzeniem wiadomości
     }
+
+    // Dołącz wątek obsługi odbierania odpowiedzi
+    receiveThread.join();
 
     closesocket(clientSocket);
     WSACleanup();
